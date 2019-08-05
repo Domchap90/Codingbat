1169dec8437e2fcc521074fa2d29cd95c56d5da8
Domchap90 <domchaple@gmail.com>
2019-08-05 21:33:30 +0100

    Upto groupSum5 working solution completed.

--- Practice/src/recursion/GroupSum.java
+++ Practice/src/recursion/GroupSum.java
@@ -4,7 +4,7 @@ public class GroupSum {
 
 	public static void main(String[] args) {
 		int array [] = {2, 4, 8};
-		System.out.println(groupSum(0, array, 10));
+		System.out.println(groupNoAdj(0, array, 7));
 	}
 	public static boolean groupSum(int start, int[] nums, int target) {
 		// base condition : every index has been checked and target has been reduced to 0 by accomplishing sum succesfully.
@@ -20,6 +20,65 @@ public class GroupSum {
 		}
 		return false;
 	}
+
+	public static boolean groupSum6(int start, int[] nums, int target) { // MUST INCLUDE 6s if they exist within array.
+		if (start >= nums.length) {
+			return (target == 0);
+		}
+		if (nums[start] == 6) {
+			if (groupSum6(start + 1, nums, target - 6)) {
+				return true;
+			}
+		} else {
+			if (groupSum6(start + 1, nums, target - nums[start])) {
+				return true;
+			}
+			if (groupSum6(start + 1, nums, target)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean groupNoAdj(int start, int[] nums, int target) {
+		// base condition : every index has been checked and target has been reduced to
+		// 0 by accomplishing sum succesfully.
+		if (start >= nums.length) {
+			return (target == 0);
+		}
+		if (groupNoAdj(start + 1, nums, target - nums[start])) {
+			return true;
+		}
+
+		if (groupNoAdj(start + 1, nums, target)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean groupSum5(int start, int[] nums, int target) {
+		if (start >= nums.length) {
+			return (target == 0);
+		}
+		if (nums[start] % 5 == 0) {
+			if (groupSum5(start + 1, nums, target - nums[start])) {
+				return true;
+			}
+		} else if (start > 0 && nums[start] == 1 && nums[start - 1] % 5 == 0) {
+			if (groupSum5(start + 1, nums, target)) {
+				return true;
+			}
+		} else {
+			if (groupSum5(start + 1, nums, target - nums[start])) {
+				return true;
+			}
+			if (groupSum5(start + 1, nums, target)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
 
 
