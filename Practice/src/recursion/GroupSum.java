b23b1deb44ebabf2f19e6a2997dec6d27af7056b
Domchap90 <domchaple@gmail.com>
2019-08-06 14:01:10 +0100

    working solution upto splitArray exercise.

--- Practice/src/recursion/GroupSum.java
+++ Practice/src/recursion/GroupSum.java
@@ -3,8 +3,8 @@ package recursion;
 public class GroupSum {
 
 	public static void main(String[] args) {
-		int array [] = {2, 4, 8};
-		System.out.println(groupNoAdj(0, array, 7));
+		int array[] = { 1, 2, 3, 10, 10, 1, 1 };
+		System.out.println(splitArray(array));
 	}
 	public static boolean groupSum(int start, int[] nums, int target) {
 		// base condition : every index has been checked and target has been reduced to 0 by accomplishing sum succesfully.
@@ -79,6 +79,48 @@ public class GroupSum {
 		return false;
 	}
 
+	public static boolean groupSumClump(int start, int[] nums, int target) {
+		if (start >= nums.length) {
+			return target == 0;
+		}
+		int i = start + 1;
+		for (; i < nums.length && nums[start] == nums[i]; i++) {
+			;// simply bumps up i to the position beyond start where the adjacent indices are
+				// no longer the same.
+		}
+		if (groupSumClump(i, nums, target - ((i - start) * nums[start]))) {// i-start is the multiplier for how many
+																			// adjacent indices there are
+			// and therefore have to all be chosen together or none chosen at all.
+			return true;
+
+		}
+
+		return groupSumClump(i, nums, target);// no 'if statement' required because there are no more conditionals
+												// afterwards resulting in the
+		// false case to be the last case scenario.
+
+	}
+
+	public static boolean splitArray(int[] nums) {
+		// base case for recursive function is where right side - left ==0 after all
+		// indices chosen.
+		// chose left or right for each element.
+		return recursSum(0, 0, 0, nums);
+	}
+
+	public static boolean recursSum(int ind, int left, int right, int[] arrayToSplitSum) {
+		if (ind >= arrayToSplitSum.length) {
+			return (right - left) == 0; // base case
+		}
+
+		if (recursSum(ind + 1, left + arrayToSplitSum[ind], right, arrayToSplitSum)) {
+			return true;
+		}
+		if (recursSum(ind + 1, left, right + arrayToSplitSum[ind], arrayToSplitSum)) {
+			return true;
+		}
+		return false;
+	}
 }
 
 
